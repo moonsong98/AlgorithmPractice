ea2d1f6b3cbf2a72319d3034ef954495db5e4d1d
Moon Kyung Song <moonsong98@postech.ac.kr>
2021-10-26 09:30:15 +0000

    10/26/2021

---
+++ baekjoon/5338.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "       _.-;;-._" << '\n';
+	cout << "'-..-'|   ||   |" << '\n';
+	cout << "'-..-'|_.-;;-._|" << '\n';
+	cout << "'-..-'|   ||   |" << '\n';
+	cout << "'-..-'|_.-''-._|" << '\n';
+}
